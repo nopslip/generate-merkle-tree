844d675ee0b190c4c0a6e8285895ac75469986bf
Zachary <1734015+nopslip@users.noreply.github.com>
2021-04-14 09:03:24 -0600

    fixing leaf hash to avoid hash collisions

--- generate_merkle_tree.js
+++ generate_merkle_tree.js
@@ -7,6 +7,7 @@ const fs = require('fs');
 const filename = 'initial_dist.csv'
 
 // create our token distribution
+const user_dist_list = []
 const token_dist = []
 
 // open csv 
@@ -14,9 +15,11 @@ fs.createReadStream(filename)
   .pipe(csv())
   .on('data', (row) => {
     // import each record line by line
-    const user_dist = [row['user_id'], row['total']]
+    const user_dist = [row['user_id'], row['total']] // create record to track user_id of leaves 
+    const user_hash = keccak256(row['user_id'], row['total']) // create a hash for each leaf as combo of user_id and amount 
     // add record to our token distribution   
-    token_dist.push(user_dist);
+    user_dist_list.push(user_dist); // used for tracking user_id of each leaf so we can write to proofs file accordingly 
+    token_dist.push(user_hash) // used for crafting the actual tree 
   })
   .on('end', () => {
     // create merkle tree from token distribution 
@@ -24,26 +27,25 @@ fs.createReadStream(filename)
     // get root of our tree 
     const root = merkle_tree.getHexRoot();
     // create proof file 
-    write_leaves(merkle_tree, token_dist, root)
+    write_leaves(merkle_tree, user_dist_list, token_dist, root)
   });
 
   // write leaves & proofs proofs to json file 
-  function write_leaves(merkle_tree, token_dist, root) {
+  function write_leaves(merkle_tree, user_dist_list, token_dist, root) {
     console.log('Begin writing leaves to file...')
     full_dist = {}
-    for (line = 0; line < token_dist.length; line++) {
-        // generate leaf hash from raw data 
+    for (line = 0; line < user_dist_list.length; line++) {
+        // generate leaf hash from raw data
         const leaf = keccak256(token_dist[line]);
         // create dist object
         const user_dist = {
             leaf: '0x' + leaf.toString('hex'),
             proof: merkle_tree.getHexProof(leaf)
         }
-        console.log(token_dist[line][0])
         // add record to our distribution 
-        full_dist[token_dist[line][0]] = user_dist;
+        full_dist[user_dist_list[line][0]] = user_dist;
     } 
-    fs.writeFile("./initial_dist_proofs.json", JSON.stringify(full_dist, null, 4), (err) => {
+    fs.writeFile("./new3_dist_proofs.json", JSON.stringify(full_dist, null, 4), (err) => {
         if (err) {
             console.error(err);
             return;
