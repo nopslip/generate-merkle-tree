cd72d54dc4775bf7eed0d920a3b5be47285a7769
Zachary <1734015+nopslip@users.noreply.github.com>
2021-04-17 19:31:25 -0600

    adding output file const

--- generate_merkle_tree.js
+++ generate_merkle_tree.js
@@ -5,7 +5,10 @@ const fs = require('fs');
 var utils = require('ethers').utils;
 
 // import distribution from this file 
-const filename = 'initial_dist.csv'
+const filename = 'token-distribution-example.csv'
+
+// what file should we write the merkel proofs too?
+const output_file = 'token-distribution-example-tree.json'
 
 // create our token distribution
 const user_dist_list = []
@@ -19,7 +22,10 @@ fs.createReadStream(filename)
     const user_dist = [row['user_id'], row['total']] // create record to track user_id of leaves 
     user_hash = utils.solidityKeccak256([ 'uint32', 'uint256'], [row['user_id'], row['total']]); // hash up the user_id and amount Solidity style    
     user_dist_list.push(user_dist); // used for tracking user_id of each leaf so we can write to proofs file accordingly 
-    token_dist.push(user_hash) // used for crafting the actual tree 
+    token_dist.push(user_hash) // used for crafting the actual tree
+    if (row['user_id'] == '9530') {
+        console.log('user_hash: ', user_hash)
+     } 
   })
   .on('end', () => {
     // create merkle tree from token distribution 
@@ -42,14 +48,17 @@ fs.createReadStream(filename)
             leaf: '0x' + leaf.toString('hex'),
             proof: merkle_tree.getHexProof(leaf)
         }
+        if (user_dist_list[line][0] == '9530') {
+          console.log('leaf hash: ', leaf.toString('hex'))
+       } 
         // add record to our distribution 
         full_dist[user_dist_list[line][0]] = user_dist;
     } 
-    fs.writeFile("./distribution_proofs.json", JSON.stringify(full_dist, null, 4), (err) => {
+    fs.writeFile(output_file, JSON.stringify(full_dist, null, 4), (err) => {
         if (err) {
             console.error(err);
             return;
         };
-        console.log("dist_proofs.json has been written with a root hash of:\n", root);
+        console.log(output_file, "has been written with a root hash of:\n", root);
     });
   }
